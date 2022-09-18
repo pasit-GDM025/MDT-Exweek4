bd192233c3efac5838022f5dfed097a02cae72a1
pasit-GDM025 <110731006+pasit-GDM025@users.noreply.github.com>
2022-09-18 21:55:53 +0700

    Add files via upload

---
+++ Ex3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class program
+{
+    static void Main(string[]args)
+    {
+        int DisasterLv;
+        Console.WriteLine("Input Disaster Level 0-7:");
+        DisasterLv = int.Parse(Console.ReadLine());
+
+       switch (DisasterLv)
+       {
+        case 1 :
+        case 3 :
+        case 5 :
+        case 7 :
+
+        Console.WriteLine("yellow");
+        break;
+       }
+
+       switch (DisasterLv)
+       {
+        case 2:
+        case 3:
+        case 6:
+        case 7:
+
+        Console.WriteLine("Orange");
+        break;
+       }
+
+       switch (DisasterLv)
+       {
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+
+        Console.WriteLine("Red");
+        break;
+       }
+    }
+}
